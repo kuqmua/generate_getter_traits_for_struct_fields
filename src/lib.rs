5da93ea820c3122b9fd84b6e046c712fa222f042
kuqmua <serezhashmakov@outlook.com>
2023-05-11 22:57:03 +0300

    renaming

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ pub fn generate_getter_traits_for_struct_fields(input: proc_macro::TokenStream) 
                 ),
             };
             let type_ident = field.ty;
-            let path_trait_ident = format!("crate::traits::fields::Get{pascal_case_field_ident}")
+            let path_trait_ident = format!("crate::traits::config_fields::Get{pascal_case_field_ident}")
                 .parse::<proc_macro2::TokenStream>()
                 .expect("path_trait_ident parse failed");
             let function_name_ident = format!("get_{field_ident}")
